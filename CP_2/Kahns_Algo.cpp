974286f9189d8cc8489fac33269369d3358fef1e
rishi058 <rishi.helloworld@gmail.com>
2023-04-19 10:00:47 +0530

    kahn's Algo

--- CP_2/Kahns_Algo.cpp
+++ CP_2/Kahns_Algo.cpp
@@ -11,6 +11,52 @@ typedef vector<int> vi;
 
 //------------------------------------------------------
 
+/*
+
+Kahn's Algorithm -->
+
+1. Mostly used in DAG (Directed Acyclic Graph).
+2. if Graph is cyclic then its topological sort isn't possible i.e (topo.size()!=n)
+3. Topological sort is an arrangement of vertices of graph such that if A is connected to B
+   then A must appear before B in the order.
+
+4. It's a Type of BFS in which all the parent node is visited before enetering the child node.
+5. It can also be used to check for cycle in a directed graph.
+
+*/
+
+vector<int> KahnAlgo(int n, vector<int> adj[]){  // (n=no. of vertices, adjacency mat)
+    
+    int indegree[n] = {0};
+    for(int i=0; i<n; i++){
+        for(int x : adj[i]){
+            indegree[x]++;
+        }
+    }
+
+    queue<int> q;
+    for(int i=0; i<n; i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+
+    vector<int> topo;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+
+        for(int x : adj[node]){
+            indegree[x]--;
+            if(indegree[x]==0){q.push(x);}
+        }
+
+    }
+
+    return topo;
+}
+
 int32_t main()
 {
     RISHI
